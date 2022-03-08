011b65084b8f33062c400a2d709fa5a21e1feabe
Shabbir&Son <Shabbir&Son@ShabbirSon-PC>
2022-03-08 15:53:32 +0500

    ddddddd

---
+++ addition first project/src/module-info.java
@@ -0,0 +1,17 @@
+module additionproject1 {
+	package dummy;
+
+	public class dummy1 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int x=23;
+			int y=20;
+			int result=x+y;
+			System.out.println("XLVII =" + result);
+			
+		}
+				
+		}
+			
+}
